ef3120e790aedbe5d991b13d5b4b0ef37adf0956
dev <78466267+tarekadel2020@users.noreply.github.com>
2022-02-16 15:02:05 +0200

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -41,17 +41,18 @@ check_conf (){
 }
 
 setting (){
-	iso=$(curl -4 ifconfig.co/country-iso)
+	#iso=$(curl -4 ifconfig.co/country-iso)
 	timedatectl set-ntp true
 	sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 	pacman -S --noconfirm reflector rsync grub
 	cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 echo -ne "
 -------------------------------------------------------------------------
-                    Setting up $iso mirrors for faster downloads
+                    Setting up mirrors for faster downloads
 -------------------------------------------------------------------------
 "
-	reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+	#reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+	reflector --sort rate -l 5 --save /etc/pacman.d/mirrorlist
 	mkdir /mnt &>/dev/null # Hiding error message if any
 }
 
